d38cacbaa50677838c6ea897c9fde8c3e650982b
olgareset <66706247+olgareset@users.noreply.github.com>
2020-09-24 19:57:26 -0400

    TestTest file modified

--- src/com/syntax/merge/TestStash.java
+++ src/com/syntax/merge/TestStash.java
@@ -4,8 +4,9 @@ public class TestStash {
 
 	public static void main(String[] args) {
 		
-		System.out.println("New code");
+		System.out.println("Modification of our code");
 
+		System.out.println("Hello world");
 	}
 
 }
